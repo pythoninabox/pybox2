da7da5f443df72d2675a5cef08f8a9154711a21b
franeum <elettrochuck@gmail.com>
2023-02-26 23:54:47 +0100

    update

---
+++ src/pybox/tone.py
@@ -0,0 +1,17 @@
+import board
+import simpleio
+
+class TONE:
+    def __init__(self):
+        self._pin = board.GP2
+
+    def play(self, note=440, duration=1):
+        simpleio.tone(self._pin, note, duration)
+
+    def mplay(self, midinote, duration=1):
+        note = mtof(midinote)
+        self.play(note, duration)
+
+
+def mtof(m):
+    return int(2**((m-69) / 12) * 440)
