3d94c64302d1fd2ef80b4a513e6e19d08f8aaf00
franeum <elettrochuck@gmail.com>
2023-03-29 12:51:41 +0200

    test keypad on button

--- src/pybox/button.py
+++ src/pybox/button.py
@@ -1,22 +1,131 @@
-from pybox.led import LED
-from pybox.button import BUTTON
-import time
+"""This module help you to manage integrated button.
 
-push = BUTTON()
-led = LED("external")
+Examples:
+    >>> from pybox.button import BUTTON
+    >>> but = BUTTON()
+    >>> but.value
+    0
+    >>> # press and hold the button
+    >>> but.value
+    1
+
+The user basically should use only the `BUTTON` class inside it.  
+"""
+
+import time
+import board
+from digitalio import DigitalInOut, Direction, Pull
+import keypad
+
+
+class BUTTON:
+    """Button class.
+    """
+    def __init__(self):
+        """
+        self.btn = DigitalInOut(board.GP6)
+        self.btn.direction = Direction.INPUT
+        self.btn.pull = Pull.UP
+        """
+        self.keys = keypad.Keys((board.GP6,), value_when_pressed=False)
+        self.previous_status = True
+        self._actions = []
+        self.press_function = None
+        self.release_function = None
+        
+    def update(self):
+        """Update button state on main loop.
+        """
+        """
+        for func in self._actions:
+            func()
+        """
+        #self.previous_status = self.btn.value
+
+        event = self.keys.events.get()
+        if event:
+            if event.pressed:
+                self.press_function()
+            elif event.released:
+                self.release_function()
+        
+    def press_handler(self, func):
+        """Add a function to call when button is pressed.
+
+        Examples:
+            >>> def when_pressed():
+                    print("PRESSED")
+            >>> push = BUTTON()
+            >>> push.press_handler(when_pressed)
+            >>> while True:
+                    push.update()
+                    # it prints PRESSED on press button
+
+        Args:
+            func (callable): a func to be executed when button is pressed. 
+
+        Todo:
+            - Add possibility to add arguments to func
+        """
+        self.press_function = func
+        self._add_handler(self.__pressed)
+    
+    def release_handler(self, func):
+        """Add a function to call when button is released.
+
+        Examples:
+            >>> def when_released():
+                    print("RELEASED")
+            >>> push = BUTTON()
+            >>> push.release_handler(when_released)
+            >>> while True:
+                    push.update()
+                    # it prints RELEASED on release button
+
+        Args:
+            func (callable): a func to be executed when button is released. 
+
+        Todo:
+            - Add possibility to add arguments to func
+        """
+        self.release_function = func
+        self._add_handler(self.__released)
+        
+    def __pressed(self):
+        if not self.btn.value:
+            if self.previous_status != self.btn.value:
+                self.press_function()
+        
+    def __released(self):
+        if self.btn.value:
+            if self.previous_status != self.btn.value:
+                self.release_function()
+                
+    def _add_handler(self, f):
+        self._actions.append(f)
+        
+    @property
+    def value(self):
+        """Raw button value.
+
+        Returns:
+            (int): button value, 1 if pressed, 0 otherwise
+        """
+        return int(not self.btn.value)
+
 
 def pre():
-    #print("PRESSEEEED :-D")
-    led.on()
-
+    print("PRESSEEEED :-D")
+    
 def rel():
-    #print("RELEASED :-(")
-    #print("time of pressure:", push.time_pressed)
-    led.off()
-
-push.press_handler(pre)
-push.release_handler(rel) 
-
-while True:
-    push.update()
-    time.sleep(0.001)
+    print("__RELEASED :-(")
+        
+if __name__=='__main__':
+        
+    push = BUTTON()
+    push.press_handler(pre)
+    push.release_handler(rel)
+    
+    while True:
+        push.update()
+        #time.sleep(0.001)
