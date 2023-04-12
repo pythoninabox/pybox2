b869890cc68212aebf0fe2aa51c3c897f090ec25
franeum <elettrochuck@gmail.com>
2023-04-12 23:35:15 +0200

    update doc

--- pybox/ring.py
+++ pybox/ring.py
@@ -24,18 +24,6 @@ from pybox.color import RED, OFF
 
 
 class ARING:
-    """Aring class.
-
-    ---
-
-    Args:
-        color: color in (r, g, b) format, tipically a pybox.color identifier [ *Default*: pybox.color.RED ].
-        brightness: value between 0.0 and 1.0 [ *Default*: 0.25 ].
-
-    Examples:
-        >>> ring = RING(color=GREEN)
-        >>> ring.write(1)
-    """
     NUMBER = 12
 
     def __init__(self, ctrl_pin=board.GP27, color: tuple[int] = RED, brightness: float = 0.25):
@@ -52,15 +40,6 @@ class ARING:
         self._np[index] = self.__parse_color(index, color)
 
     def write(self, col: tuple = None) -> None:
-        """Manage all the ring globally.
-
-        Args:
-            col (tuple | int): if a `tuple` set `full_color` property and use it to turn on/off the whole Ring, if a non-zero `int` turn on the whole Ring using `full_property`. If zero, turn off it with `pybox.color.OFF`
-
-        Examples:
-            >>> ring.write(1)   # turn on all the ring with current global color
-            >>> ring.write(0)   # turn off all the ring
-        """
         if col is not None:
             if isinstance(col, tuple):
                 if col != OFF:
@@ -76,25 +55,12 @@ class ARING:
         self._np.fill(_col)
 
     def on(self) -> None:
-        """Turn on the ring globally.
-
-        Examples:
-            >>> ring.on()   # turn on all the ring with current global color
-        """
         self.write(1)
 
     def off(self) -> None:
-        """Turn off the ring globally.
-
-        Examples:
-            >>> ring.off()  # turn off the ring
-        """
         self.write(0)
 
     def toggle(self) -> None:
-        """Turn on the ring globally if the first Pixel is currently turned off and viceversa.
-
-        """
         if self._np[0] == OFF:
             self.write(1)
         else:
@@ -102,16 +68,6 @@ class ARING:
 
     @property
     def color(self) -> tuple[int]:
-        """Get or set the global color used in `write` method
-
-        Args:
-            col (tuple[int]): color in tuple format, you can use pybox.color identifier. [ *Default*: `pybox.color.RED` ].
-
-        Examples:
-            >>> ring.color = CYAN   # set global color to pubox.color.CYAN
-            >>> ring.color          # get global color
-        """
-
         return self._global_col
 
     @color.setter
@@ -120,11 +76,6 @@ class ARING:
 
     @property
     def brightness(self) -> float:
-        """Get or set brightness (only at global level)
-
-        Args:
-            value (float): float between 0.0 (min brightness) and 1.0 (max brightness)
-        """
         return self._np.brightness
 
     @brightness.setter
@@ -132,46 +83,15 @@ class ARING:
         self._np.brightness = value
 
     def __setitem__(self, index: int, item: tuple[int]) -> None:
-        """Set a pixel as list item
-
-        Description: 
-            a Ring is basically a list of Pixel. At a lower level you can set a single (or a group of) 
-            Pixel(s) using slice notation. Preferably you shoud use `set` method
-
-        Args:
-            index: pixel index [ *Required* ]
-            item: tuple for RGB desc [ *Default*: `None`]
-
-        Examples:
-            >>> ring[0] = GREEN
-            >>> ring[1:3] = [GREEN, GREEN]
-        """
         self._np[index] = self.__parse_color(index, item)
 
     def __getitem__(self, index: int) -> tuple[int]:
-        """Get color at index.
-
-        Args:
-            index: Pixel index
-
-        Examples:
-            >>> ring[0] = GREEN     # set first Pixel
-            >>> ring[0]             # get first Pixel
-            (0, 255, 0)
-        """
         return self._np[index]
 
     def set_pixel_color(self, index: int = None, col: tuple = None) -> None:
-        """Set single Pixel color.
-
-        Args:
-            index: position index of Pixel
-            col: color in (r, g, b) color. Tipically an identifier from `pybox.color` [ *Default*: `None` ].
-        """
         self._col[index] = col
 
         if isinstance(index, int):
-            # self.__set_single_led(index, col)
             self._col[index] = col
         else:
             try:
@@ -184,37 +104,12 @@ class ARING:
                     "Please, provide an int, a tuple, a list or a range as first arg") from exc
 
     def get_pixel_color(self, index: int = None) -> tuple[int]:
-        """Get single Pixel color.
-        """
         return self._col[index]
 
     def is_on(self, index: int = None) -> bool:
-        """Verify if a Pixel is on
-
-        Args:
-            index (int, optional): index of the pixel to check. Defaults to None.
-
-        Returns:
-            bool: True if the pixel at index is on, otherwise False
-        """
         return self._np[index] != OFF
 
     def set_pixel(self, index: int = None, col: tuple[int] = None):
-        """set color of a (group of) Pixel(s).
-
-        You can set a single Pixel with a int number. Alternatively you can set a group of Pixels with a list (or tuple) 
-        or using a range instance.
-
-        Args:
-            index (int, list, tuple, range): index of Pixel(s) [ *Required* ]
-            col (tuple[int], optional): _description_. [ *Default*: `None` ]
-
-        Examples:
-            >>> ring.set_pixel(0, GREEN)            # turn on the Pixel at index 0 (in green)
-            >>> ring.set_pixel([0,1,2], BLUE)       # turn on first three Pixels (in blue)
-            >>> ring.set_pixel(range(0,12,2), RED)  # turn on all Pixels at even positions (in red)
-            >>> ring.set_pixel(0, OFF)              # turn off the Pixel at index 0
-        """
         if isinstance(index, int):
             self.__set_single_led(index, col)
             return None
@@ -247,12 +142,14 @@ class ARING:
 class PIXEL:
     """Pixel class.
 
-        You don't create a PIXEL instance directly, but when you create a RING object, it creates a list of PIXEL objects.
+        A PIXEL object is an abstraction to manage a single rgb led, part of a RING object. 
 
         Args:
             index (int): index of the pixel in the ring class list. Defaults to None.
             color (tuple[int], optional): color of the Pixel. Defaults to RED.
             ring (ARING, optional): Ring reference. Defaults to None.
+
+        You don't create a PIXEL instance directly, but when you create a RING object, it creates a list of PIXEL objects.
 
         Examples:
             >>> ring = RING()
@@ -322,6 +219,8 @@ class RING:
     """Ring class.
 
     ---
+
+    Manage the ring of 12 rgb leds on board of pybox
 
     Args:
         color: color in (r, g, b) format, tipically a pybox.color identifier [ *Default*: pybox.color.RED ].
